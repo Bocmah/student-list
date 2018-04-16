f03fda6dd1f96b5b6a723a06980dad16448aaa98
Artem <atrushkin1@gmail.com>
2018-04-16 11:39:02 +0300

    Fixed path to css files

--- views/partials/header.php
+++ views/partials/header.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Student List </title>
-    <link rel="stylesheet" href="uikit/css/uikit.min.css">
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="../uikit/css/uikit.min.css">
+    <link rel="stylesheet" href="../css/styles.css">
 </head>
 <body>
