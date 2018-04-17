1615c84db297ad46ed8c46fe691c0cbecfc9afb7
Artem <atrushkin1@gmail.com>
2018-04-17 11:44:11 +0300

    Fixed alert message block

--- views/partials/success-message.php
+++ views/partials/success-message.php
@@ -1,4 +1,6 @@
-<div class="uk-alert-success uk-align-center" uk-alert>
-    <a class="uk-alert-close" uk-close></a>
-    <p>Вы зарегистрированы. При желании можете <a href="/profile/edit">отредактировать</a> профиль.</p>
+<div class="uk-container uk-container-small">
+    <div class="uk-alert-success uk-margin-small-bottom" uk-alert>
+        <a class="uk-alert-close" uk-close></a>
+        <p>Вы зарегистрированы. При желании можете <a href="/profile/edit">отредактировать</a> профиль.</p>
+    </div>
 </div>
