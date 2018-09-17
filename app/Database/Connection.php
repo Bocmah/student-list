245219a47936f6541e182633a03cc92550d4412e
Artem <atrushkin1@gmail.com>
2018-09-17 15:00:39 +0300

    Added encoding in the config

--- app/Database/Connection.php
+++ app/Database/Connection.php
@@ -13,10 +13,11 @@ class Connection
     {
         try {
             return new \PDO(
-                $config["database"]['connection'].';dbname='.$config["database"]['name'],
-                $config["database"]['username'],
-                $config["database"]['password'],
-                $config["database"]['options']
+                $config["database"]["connection"].";dbname=".$config["database"]["name"].
+                ";charset=".$config["database"]["encoding"],
+                $config["database"]["username"],
+                $config["database"]["password"],
+                $config["database"]["options"]
             );
         } catch (\PDOException $e) {
             die($e->getMessage());
--- config.php
+++ config.php
@@ -6,6 +6,7 @@ return [
         "username" => "root",
         "password" => "",
         "connection" => "mysql:host=127.0.0.1",
+        "encoding" => "utf8",
         "options" => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         ]
